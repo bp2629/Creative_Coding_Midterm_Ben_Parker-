b13075c6e4ecf69c45b7b6441d429b970e45115a
Ben Parker <bp2629@nyu.edu>
2025-11-12 21:14:47 -0500

    Order Events, Clean Code

--- Guy.js
+++ Guy.js
@@ -5,11 +5,11 @@ class Guy {
     this.clickX = this.x
     this.clickY = this.y
     this.speed = 8; // starter speed.
-    this.speed_change = 1
+    this.speed_change = 1 //decrease amount. 
     this.color = 0
   }
 
-  moveTo(x, y) {
+  moveTo(x, y) { // where you click is where the dot goes. And the speed decreases accordingly. 
     this.clickX = x
     this.clickY = y
     this.speed =  this.speed - this.speed_change
@@ -17,29 +17,34 @@ class Guy {
     
   }
 
-  update() {
+  update() { // go in a straight line to where you clicked. 
     let distanceX = this.clickX - this.x
     let distanceY = this.clickY - this.y
     let distance = Math.sqrt(distanceX*distanceX + distanceY*distanceY); //Pythagorieam Triangle Math --> To make a new line to mouse click loaction. 
 
     if (distance == 0){
       distance = 1
-    }; // CAN'T DIVIDE BY ZERO 
+    }; // CAN'T DIVIDE BY ZERO  
 
     this.x += (distanceX / distance) * this.speed  //Go there at the speed in both x and y 
     this.y += (distanceY / distance) * this.speed
 
 
-    if (this.speed <= 0){
-      this.speed_change = 0
+    if (this.speed <= 0){ //Fade out when it has completly slowed down//passed out. 
+      this.speed_change = 0 
       this.color += 10
     }
   }
 
 
-
-  display() {
+  display() {  //the dot/ the guy
     fill(255,this.color,this.color)
     ellipse(this.x, this.y, 40, 40)
   }
 }
+
+
+
+/// This link, even though its solving a slightly seperate problem in a different way, helped me with this part.
+// *** Particularly the image because It made me occur why my dot wasn't going to the correct place. Made with think of A^2 + b^2 = c^2 phythagorian. 
+//https://stackoverflow.com/questions/70267458/how-do-i-make-a-object-move-towards-the-mouse-in-p5-js 
--- sketch.js
+++ sketch.js
@@ -1,201 +1,121 @@
-
-
 let r = 10
 let sleepr = 0
 let sleepg = 0
 let sleepb = 0
 let counter = 0
-let bc = (26, 0, 41)
-let click = 0
-
 let snooze_counter = 0 
-
 let eyes_counter = 0
-
 let time = 0
-
-
 let starsx = new Array(30)
 let starsy = new Array(30)
-
 let alarmc = 255
-
 let g;
 
-
 function setup() {
- // put setup code here
-
- frameRate(30)
 
  createCanvas(500,500);
  background(26, 0, 41); 
+
  g = new Guy()
 
  yawn1 = new Yawn(0,0,.5)
  yawn2 = new Yawn(100,100,.5)
-
-
-
-
-
-
-
- 
 }
  
 
 
 function draw() {
-  // put drawing code here
-  // background(150)
-   counter += 1
-
-   time += 1 
-
-   
-
-  print(counter)
-
-  stars()
+   counter += 1 //time used in some antimations. 
+   time += 1  //standard time for entire animation to run. 
 
 
 
-  // if (time <= 150){
+   //TIMELINE
+  if (time <= 150){
+    sleeping()
+  }
 
-  //   sleeping()
+  else if (time <= 200 ){
+    alarm(counter)
+  }
 
-  // }
+  else if (time <= 300){
+    sleeping()
+  }
 
-  // else if (time <= 200 ){
+  else if (time <= 350){
+    alarm(counter)
+  }
 
-  //   alarm(counter)
+  else if (time <= 550){
+    snooze_counter += 1 
 
-  // }
+    snooze(snooze_counter)
+  }
 
-  // else if (time <= 300){
+  else if (time <= 1180){
+    eyes_counter +=1 
+    eyesopen(eyes_counter)
+  }
 
-  //   sleeping()
-
-  // }
-
-  // else if (time <= 350){
-
-  //   alarm(counter)
-
-  // }
-
-
-  // else if (time <= 550){
-
-  //   snooze_counter += 1 
-
-  //   snooze(snooze_counter)
-
-  // }
-
-  // else if (time <= 780){
-
-  //   eyes_counter +=1 
-  //   eyesopen(eyes_counter)
-
-  // }
-
-  // else if (time <= 950){
-
-  //   coffee((random(100,275)))
-
-  // }
+  else if (time <= 1350){
+    coffee((random(100,275)))
+  }
 
 
-  // else if (time <= 1800){
-    
-  //   maze()
-  //   g.update()
-  //   g.display();
-  // }
+  else if (time <= 2600){
+    maze()
+    g.update()
+    g.display();
+  }
 
   
 
-  // else if (time <= 2500){
-
-  //   background(0)
-
-  //   stars()
-  // }
-  
-
-
-
-
-  
-
-
-
-
-   //sleeping()
-   //alarm(counter)
-   // sleeping()
-   //alarm(counter)
-   //snooze(counter)
-   //eyesopen(counter)
-   // coffee((random(100,275)))
-  // maze()
-  //g.update()
- // g.display();
-   // yawn1.update()
-  // yawn1.display()
-
-  // yawn2.update()
-  // yawn2.display()
-  // stars()
+  else if (time <= 3100){
+    background(0)
+    stars()
+  }
 
 }
 
+
+
+
+
+
+
+// FUNCTIONS BELOW 
+
+
+
+
+
+
+
+
 function stars(){
-
   frameRate(5)
-
   background(0)
 
-
-
-  starsx[0] = random(500)
+  starsx[0] = random(500) //Make first slot random position for star. 
   starsy[0] = random(500)
   
 
   for(let i = 0; i < starsx.length ; i++){ 
     fill(255-i*10)
-    ellipse(starsx[i], starsy[i], 10, 10);
+    ellipse(starsx[i], starsy[i], 10, 10); //make fading stars. 
 }
 
 for(let i = starsx.length - 1;  i > 0; i-- ){
   starsx[i] = starsx[i-1];
-  starsy[i] = starsy[i-1];
+  starsy[i] = starsy[i-1]; //cycle through fading stars. 
 }
-
-
 }
-
-
-
-
-
 
 function coffee(drip){
 
   let pos = 100
-
- 
-
-  
-
- 
-
-  
-
   background(0)
-
 
   fill(255) //cup
   ellipse(width/2, height/2+50, 375, 400)
@@ -211,16 +131,6 @@ function coffee(drip){
 
   fill(0) // background to create cup shape. 
   rect(0,50, width, height/2)
-
-
-
-
-  
-
-  
-
-
-
   fill(155) // spout
   rect(width/2-20,0, 40, 80)
 
@@ -243,7 +153,7 @@ function maze(){
   background(0)
   
   stroke(255)
-  strokeWeight(65)
+  strokeWeight(65) //single line creating maze. Left to right. 
 
   line(0,height/2,width/2,height-120)
   line(width/2,height-120, 65,65)
@@ -253,33 +163,14 @@ function maze(){
   line( width/ 2 -50, height/2 -200,width-20, height/2 +200 )
   line(width-20, height/2 +200 , width-70, height/2 )
   line( width-70, height/2 , height, height/2 )
-  stroke(0)
+
+  stroke(0) //Reset
   strokeWeight(0)
-  
-
-  // beginShape();
-  // vertex(width/2,0);
-  // vertex(width/2,425);
-  // vertex(width/2 -50 ,425);
-  // vertex(width/2 -50 ,0);
-
-
-  // // vertex(,);
-  // // vertex(,);
-  // // vertex(,);
-  // // vertex(,);
-
-
-  // endShape(CLOSE)
-
 }
 
 
 function mousePressed(){
-
-  g.moveTo(mouseX,mouseY)
-
-
+  g.moveTo(mouseX,mouseY) // Guy movement. 
 }
 
 
@@ -287,14 +178,14 @@ function sleeping(){
 
   fill(sleepr,sleepg,sleepb)
   stroke(0)
-  ellipse(width/2,height/2, r,r)
+
+  ellipse(width/2,height/2, r,r) //growing circle. 
 
   r += 20
 
-  if (r > width * 2){
-    
+  if (r > width * 2){   //alternate between purple and black. 
     r = 10 
-    
+
     if (sleepr == 0){ 
       sleepr = 26
       sleepb = 41
@@ -314,7 +205,7 @@ function sleeping(){
 function alarm(counter){
 
 
-  if (counter % 10 === 0){
+  if (counter % 10 == 0){ //alternate between red and black backroound rectangle every 10
     alarmc = 0
   }
 
@@ -323,21 +214,16 @@ function alarm(counter){
   }
   fill(alarmc,0,0)
   rect(0,0,width,height)
-
-
-
-
 }
 
 
 function eyesopen(counter){
 
-  counter = counter/ 7
-
+  counter = counter/ 7  //slow down speed. 
   background(0)
 
   
-  if (counter <= 40) {
+  if (counter <= 40) { //open eyes
 
     open = counter
   }
@@ -345,10 +231,7 @@ function eyesopen(counter){
 
     open = 40
   }
-
-
-
-  if (counter <= 20) {
+  if (counter <= 20) { // dilate pupils 
 
     pupil = counter
   }
@@ -357,55 +240,35 @@ function eyesopen(counter){
     pupil = 20
   }
 
-  if (counter>45){
+  if (counter>45){ //close eyes 
     open -= (counter-45)*5
   }
 
-  if (counter>53){
+  if (counter>53){ //open quickly 
     open = 40
   }
 
-  if (counter>60){
-    open -= (counter-60)*5
+  if (counter>60){ //close again. 
+    open -= (counter-60)*5 
   }
 
-  if (counter>68){
+  if (counter>68){ // quick open. 
     open = 40
   }
 
-  if (counter>72){
+  if (counter>72){  //blinking for the next 3
     open = 0
   }
-  if (counter>73){
+  if (counter>73){ 
     open = 40
   }
-  if (counter>78){
+  if (counter>78){ 
     open = 0
   }
   if (counter>79){
     open = 40
   }
 
-  
-
-
-  // if (counter>55){
-  //   open = 40
-  // }
-
-  // if (counter>65){
-  //   open = 0
-  // }
-  // if (counter>70){
-  //   open = 40
-  // }
-  // if (counter>75){
-  //   open = 0
-  // }
-  // if (counter>80){
-  //   open = 40
-  // }
-   
   fill(255, 0, 0, 128)
   ellipse(width/4,width/2,200,open) // left eye
   fill(0)
@@ -416,24 +279,18 @@ function eyesopen(counter){
   ellipse(3*width/4,width/2,200,open) //  right eye 
   fill(0)
   ellipse(3*width/4,width/2,20,pupil)
-
 }
-
-
 
 function snooze(counter){
   
+  background(255,0,0)
 
-   background(255,0,0)
-
-  
-
-  let snooze_r = 255 
+  let snooze_r = 255 //orange color. 
   let snooze_b = 179 
   let snooze_g = 0
 
 
-  if (counter <= 75){
+  if (counter <= 75){ //start with button on, finger off screen but moving foward. 
 
     fill(0)
     rect(100,0, 300,height,25) //screen
@@ -442,41 +299,34 @@ function snooze(counter){
     fill(150)
     rect(225,400,50,25,15) // other button
   
-    fill(255, 225, 176)
+    fill(255, 225, 176) //finger
     stroke(0)
-    rect(-2000 + counter * 20,200,800,125,50)
+    rect(-2000 + counter * 20,200,800,125,50) 
 
   }
-  else if(counter > 75){
+  else if(counter > 75){ //fade buttons, retract finger. 
 
     fill(0)
     rect(100,0, 300,height,25) //screen
   
-    let extra_button = 150 - (counter - 75) * (counter/75)
+    let extra_button = 150 - (counter - 75) * (counter/75)  //fade out other button. 
     fill(extra_button)
     rect(225,400,50,25,15) // other button
 
 
 
-    let snooze_r = 255 - (counter - 75) * (counter/75)
+    let snooze_r = 255 - (counter - 75) * (counter/75) //fade out snooze button. 
     let snooze_b = 179 - (counter - 75)  * (counter/75)
     fill(snooze_r, snooze_b, snooze_g)
     rect(125,200,250,75,15) //snooze button
 
 
-     fill(255, 225, 176)
+    fill(255, 225, 176)
     stroke(1)
     rect(250 -counter * 10  ,200,800,125,50)
 
 
   }
-
-
-
-
-print(counter)
-
-
 }
 
 
